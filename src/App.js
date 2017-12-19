5409198d477245e4700a9348eb8a8a695415c673
Roxie Charchian <rmcharchian@gmail.com>
2017-12-19 06:41:28 -0600

    added a bit of styling

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ render() {
     <div className="App">
       <div>Countdown to December 25, 2017</div>
       <div>
-        <div>14 Days</div>
-        <div>30 Hours</div>
-        <div>15 Minutes</div>
-        <div>20 Seconds</div>
+        <div className="Clock-days">14 Days</div>
+        <div className="Clock-hours">30 Hours</div>
+        <div className="Clock-minutes">15 Minutes</div>
+        <div className="Clock-minutes">20 Seconds</div>
       </div>
       <div>
       <input placeholder='new date' />
