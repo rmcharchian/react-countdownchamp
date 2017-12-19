dbb6ab95e1fd56e5a3d7772e601c63490cd42139
Roxie Charchian <rmcharchian@gmail.com>
2017-12-19 17:11:06 -0600

    updating console log with  seconds minutes hours day

--- src/Clock.js
+++ src/Clock.js
@@ -10,13 +10,25 @@ class Clock extends Component {
       minutes: 0,
       seconds: 0
     }
-    console.log('this.props', this.props);
+  }
+
+  getTimeUntil(deadline) {
+    const time = Date.parse(deadline) - Date.parse(new Date());
+    console.log('time', time);
+    const seconds = Math.floor((time/1000)%60);
+    const minutes = Math.floor((time/1000/60)%60);
+    const hours = Math.floor(time/(1000*60*60)%24);
+    const days = Math.floor(time/(1000*60*60*24));
+
+    console.log('seconds', seconds, 'minutes', minutes, 'hours', hours, 'days', days);
   }
 
   render(){
+    this.getTimeUntil(this.props.deadline);
+
     return (
     <div>
-      <div className="Clock-days">{this.state.days} days</div>
+      <div className="Clock-days">{this.state.days} days </div>
       <div className="Clock-hours">{this.state.hours} hours</div>
       <div className="Clock-minutes">{this.state.minutes} minutes</div>
       <div className="Clock-minutes">{this.state.seconds} seconds</div>
