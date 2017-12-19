98c23841efe8cee488b9a7e06ccc1f895a7db3b4
Roxie Charchian <rmcharchian@gmail.com>
2017-12-19 12:06:24 -0600

    new deadline updates as entered

--- src/App.js
+++ src/App.js
@@ -5,12 +5,15 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      deadline: 'December 25, 2017'
+      deadline: 'December 25, 2017',
+      newDeadline: ''
     };
   }
 
 changeDeadline() {
-  this.setState({deadline: 'November 25, 2017'})
+  // this.setState({deadline: 'November 25, 2017'})
+  console.log('state', this.state);
+  this.setState({deadline: this.state.newDeadline});
 }
 
 render() {
@@ -24,7 +27,10 @@ render() {
         <div className="Clock-minutes">20 Seconds</div>
       </div>
       <div>
-      <input placeholder='new date' />
+      <input
+        placeholder='new date'
+        onChange={event => this.setState({newDeadline: event.target.value})}
+        />
       <button onClick={() => this.changeDeadline()}>
       Submit
       </button>
