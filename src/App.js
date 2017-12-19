d876144bf31d361c185ee12d0ab39468e6e5f58b
Roxie Charchian <rmcharchian@gmail.com>
2017-12-18 19:04:49 -0600

    added components to app.js and added css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
+import './App.css';
 
 class App extends Component {
 render() {
   return (
-    <div>Countdown Champ, App Component</div>
+    <div className="App">
+      <div>Countdown to December 25, 2017</div>
+      <div>
+        <div>14 Days</div>
+        <div>30 Hours</div>
+        <div>15 Minutes</div>
+        <div>20 Seconds</div>
+      </div>
+      <div>
+      <input placeholder='new date' />
+      <button>Submit</button>
+      </div>
+    </div>
     )
   }
 }
 
 
 export default App;
- 
