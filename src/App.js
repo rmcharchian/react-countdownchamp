58cea119c860b15a0c1ac3f6e2a676a3e7c6b095
Roxie Charchian <rmcharchian@gmail.com>
2017-12-19 11:54:37 -0600

    updated state

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ class App extends Component {
     };
   }
 
+changeDeadline() {
+  this.setState({deadline: 'November 25, 2017'})
+}
+
 render() {
   return (
     <div className="App">
@@ -21,7 +25,9 @@ render() {
       </div>
       <div>
       <input placeholder='new date' />
-      <button>Submit</button>
+      <button onClick={() => this.changeDeadline()}>
+      Submit
+      </button>
       </div>
     </div>
     )
